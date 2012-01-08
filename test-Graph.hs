0382e3eb79152097fd71f546bc4225bf8e74aa5e
odf <olaf.delgado@gmail.com>
2012-01-08 17:57:00 +1100

    Added some tests and tried (unsuccessfully) to use test-framework.

--- test-Graph.hs
+++ test-Graph.hs
@@ -1,5 +1,21 @@
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck (testProperty)
+
 import Test.QuickCheck
+import Test.HUnit
+
+import Data.List (sort)
 import Graph
+
+main = defaultMain tests
+
+tests = [
+  testGroup "Basics" [
+      testProperty "vertices" (prop_vertices :: Graph Int -> Bool)
+    , testProperty "edges"    (prop_edges    :: Graph Int -> Bool)
+    ]
+  ]
 
 instance (Ord a, Arbitrary a) => Arbitrary (Graph a) where
   arbitrary = do
@@ -11,3 +27,16 @@ instance (Ord a, Arbitrary a) => Arbitrary (Graph a) where
       else do
         edges <- listOf $ elements allEdges
       	return (foldl insert g edges)
+
+prop_vertices :: (Ord a) => Graph a -> Bool
+prop_vertices g = all (`elem` verts) ends
+  where verts = vertices g
+        ends  = concat [map Vertex [v, w] | (Edge v w) <- edges g]
+
+prop_edges :: (Ord a) => Graph a -> Bool
+prop_edges g = all (== edgesInOrder) [succEdges, predEdges]
+  where edgesInOrder = sort $ edges g
+        succEdges    = sort [edge v w | v <- vertices g, w <- succs g v]
+        predEdges    = sort [edge v w | w <- vertices g, v <- preds g w]
+        edge v w     = Edge (Graph.label v) (Graph.label w)
+
